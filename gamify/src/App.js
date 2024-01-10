5f60a8fc19c62ea76c194bd91970d5b37f04eb77
Samaira Stewart <stewartsamaira@gmail.com>
2024-01-10 00:49:19 -0500

    Added avatar images

--- gamify/src/App.js
+++ gamify/src/App.js
@@ -1,6 +1,6 @@
 // import InfoPage from './navPages/InfoPage';
 import SignInPage from './navPages/SignInPage';
-import NavBar from './navPages/Navbar.js';
+import NavBar from './components /Navbar.js';
 import React from 'react';
 import RegisterPage from './navPages/RegisterPage.js';
 import AvatarPage from './navPages/AvatarPage.js';
